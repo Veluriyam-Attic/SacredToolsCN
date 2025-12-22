b0ebd6506739441dbe107bd2d7ccbdf1a97da78c
Henceforth <3412615165@qq.com>
2025-12-22 16:02:23 +0800

    Sync

--- SacredToolsCN.cs
+++ SacredToolsCN.cs
@@ -1,14 +1,16 @@
 using Terraria.ModLoader;
 using TigerForceLocalizationLib.Filters;
 using TigerForceLocalizationLib;
+using Terraria.GameContent;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace SacredToolsCN
 {
 	public class SacredToolsCN : Mod
 	{
-        public override void PostSetupContent()
-        {
+		public override void PostSetupContent()
+		{
 			TigerForceLocalizationHelper.LocalizeAll("SacredToolsCN", "SacredTools", false);
-        }
+		}
 	}
 }
