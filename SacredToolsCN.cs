ac4b318dbf2a068c85b92224693cca8c1428e063
Henceforth <3412615165@qq.com>
2025-12-22 13:19:28 +0800

    Using tiger to replace texture

--- SacredToolsCN.cs
+++ SacredToolsCN.cs
@@ -1,15 +1,14 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Terraria.ModLoader;
+using TigerForceLocalizationLib.Filters;
+using TigerForceLocalizationLib;
 
 namespace SacredToolsCN
 {
-	// Please read https://github.com/tModLoader/tModLoader/wiki/Basic-tModLoader-Modding-Guide#mod-skeleton-contents for more information about the various files in a mod.
 	public class SacredToolsCN : Mod
 	{
-
+        public override void PostSetupContent()
+        {
+			TigerForceLocalizationHelper.LocalizeAll("SacredToolsCN", "SacredTools", false);
+        }
 	}
 }
